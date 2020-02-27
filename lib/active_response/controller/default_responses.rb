8925a9ec7128ef62e3d9101d9346ac4998e56893
Arthur Dingemans <arthur@argu.co>
2020-02-27 15:36:10 +0100

    Fix deprecation warnings

--- lib/active_response/controller/default_responses.rb
+++ lib/active_response/controller/default_responses.rb
@@ -4,35 +4,35 @@ module ActiveResponse
   module Controller
     module DefaultResponses
       def respond_with_collection(opts)
-        active_responder.collection(opts)
+        active_responder.collection(**opts)
       end
 
       def respond_with_destroyed(opts)
-        active_responder.destroyed(opts)
+        active_responder.destroyed(**opts)
       end
 
       def respond_with_form(opts)
-        active_responder.form(opts)
+        active_responder.form(**opts)
       end
 
       def respond_with_invalid_resource(opts)
-        active_responder.invalid_resource(opts)
+        active_responder.invalid_resource(**opts)
       end
 
       def respond_with_new_resource(opts)
-        active_responder.new_resource(opts)
+        active_responder.new_resource(**opts)
       end
 
       def respond_with_redirect(opts)
-        active_responder.redirect(opts)
+        active_responder.redirect(**opts)
       end
 
       def respond_with_resource(opts)
-        active_responder.resource(opts)
+        active_responder.resource(**opts)
       end
 
       def respond_with_updated_resource(opts)
-        active_responder.updated_resource(opts)
+        active_responder.updated_resource(**opts)
       end
     end
   end
